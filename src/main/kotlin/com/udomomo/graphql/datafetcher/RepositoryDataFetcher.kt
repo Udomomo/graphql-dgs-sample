694ca883d983038d8c0efd006162e0a12340ef86
Udomomo <udomomo@gmail.com>
2025-12-31 18:07:09 +0900

    add owner dgsData in RepositoryDataFetcher

--- src/main/kotlin/com/udomomo/graphql/datafetcher/RepositoryDataFetcher.kt
+++ src/main/kotlin/com/udomomo/graphql/datafetcher/RepositoryDataFetcher.kt
@@ -1,14 +1,11 @@
 package com.udomomo.graphql.datafetcher
 
-import com.netflix.dgs.codegen.generated.types.Issue
 import com.netflix.dgs.codegen.generated.types.Repository
 import com.netflix.dgs.codegen.generated.types.User
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsData
 import com.netflix.graphql.dgs.DgsDataFetchingEnvironment
 import com.netflix.graphql.dgs.DgsQuery
-import com.netflix.graphql.dgs.InputArgument
-import com.udomomo.graphql.domain.IssueStatus
 import com.udomomo.graphql.query.IssueQuery
 import com.udomomo.graphql.query.RepositoryQuery
 import com.udomomo.graphql.query.UserQuery
@@ -21,7 +18,7 @@ class RepositoryDataFetcher(
 ) {
     @DgsQuery
     fun repository(owner: String, name: String): Repository? {
-        val userId = userQuery.findBy(owner)?.id ?: return null
+        val userId = userQuery.findByName(owner)?.id ?: return null
         return repositoryQuery.findBy(name, userId) ?.let {
             Repository(
                 id = { it.id },
@@ -34,20 +31,15 @@ class RepositoryDataFetcher(
         }
     }
 
-    // repositoryクエリでissueフィールドが指定されたときに実行される。
-    // 指定がなければ実行されない。
     @DgsData(parentType = "Repository")
-    fun issue(@InputArgument(name = "number") issueNumber: Int, dfe: DgsDataFetchingEnvironment): Issue? {
-        val repository = dfe.getSource<Repository>() ?: return null
-        return issueQuery.findBy(repository.id, issueNumber)?.let {
-            Issue(
-                id = { it.id },
-                title = { it.title },
-                number = { it.number },
-                closed = { when (it.status) {
-                    IssueStatus.OPEN -> false
-                    IssueStatus.CLOSED -> true
-                } },
+    fun owner(dfe: DgsDataFetchingEnvironment): User? {
+        val userId = dfe.getSource<Repository>().owner.id
+        val user = userQuery.findById(userId)
+        return user?.let {
+            User(
+                id = { it.id } ,
+                name = { it.name },
+                projectV2 = { null }
             )
         }
     }
